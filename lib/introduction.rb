7adc180faa808dea83505c20f99256d88442763f
keemo <keemowatson1@gmail.com>
2019-11-07 17:42:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 
